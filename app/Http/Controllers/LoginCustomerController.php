a4c9b65f6a9258a047c7e038ba1df3f6b9755cea
aaidilz <aidil.233040059@mail.unpas.ac.id>
2024-07-03 19:36:15 +0700

    refactor: Update LoginCustomerController to handle customer login and logout

--- app/Http/Controllers/LoginCustomerController.php
+++ app/Http/Controllers/LoginCustomerController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class LoginCustomerController extends Controller
 {
-    //
+    public function showLoginForm()
+    {
+        return view('login.login-customer');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+
+        if (Auth::guard('customer')->attempt($credentials)) {
+            return redirect()->intended('/customer');
+        }
+
+        return back()->with('error', 'Login failed! Please check your username and password.');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard()->logout();
+        Auth::guard('customer')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
+    }
 }
